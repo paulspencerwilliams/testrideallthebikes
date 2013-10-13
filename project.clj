ab0f819436bdef6db890dd62e1a75a9a057ddcd5
Paul Williams <will@fred.lan>
2013-10-13 18:22:11 +0100

    added lein pre-requisite in project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
 (defproject testrideallthebikes "0.1.0-SNAPSHOT"
   :description "A service for matching prospective bike buyers with shops that offer demo bikes"
   :url "http://testrideallthebik.ies"
+  :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [compojure "1.1.5"]
                  [hiccup "1.0.3"]]
